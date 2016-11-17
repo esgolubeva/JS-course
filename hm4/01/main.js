1c404d1b2d015de06ff873c54567ee4bff0af99d
Elena Golubeva <lena19948@gmail.com>
2016-11-17 20:32:58 +0300

    hm4_2

--- hm4/01/main.js
+++
@@ -1,6 +0,0 @@
-function prepend(container, element) {
-    if (!container.children.length) {
-        return container.appendChild(element);
-    }
-    return container.insertBefore(element, container.firstChild);
-}
