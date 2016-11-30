1d1fcf7b82e5b0669290c0a5b0e409877e2ef7a1
Elena Golubeva <lena19948@gmail.com>
2016-11-30 18:33:10 +0300

    added delegation

--- hm7/01/main.js
+++ hm7/01/main.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.cookie = "someName0=123; path=/; expires=Sun, 25 Aug 2019 21:13:01 GMT";
-    document.cookie = "someName1=234; path=/; expires=Sun, 25 Aug 2019 21:13:01 GMT";
-    document.cookie = "someName2=345; path=/; expires=Sun, 25 Aug 2019 21:13:01 GMT";
-    document.cookie = "someName3=456; path=/; expires=Sun, 25 Aug 2019 21:13:01 GMT";
+    document.cookie = "someName0=123; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
+    document.cookie = "someName1=234; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
+    document.cookie = "someName2=345; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
+    document.cookie = "someName3=456; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
 
     var cookieArray = document.cookie.split('; ');
 
@@ -11,7 +11,9 @@ document.addEventListener('DOMContentLoaded', function() {
         var row = document.createElement('tr');
         var delButton = document.createElement('button');
 
-        delButton.innerHTML = 'Удалить';
+        delButton.setAttribute('class', 'del-button');
+        delButton.setAttribute('data-cookie-name', cookieItem[0]);
+        delButton.innerHTML = 'удалить';
         tbody.appendChild(row);
 
         cookieItem.forEach(function(i) {
@@ -23,14 +25,21 @@ document.addEventListener('DOMContentLoaded', function() {
         var cell = document.createElement('td');
         row.appendChild(cell);
         cell.appendChild(delButton);
-
-        delButton.addEventListener('click', function() {
-            var cookieName = cookieItem[0];
-            var date = new Date(0);
-            if (confirm("Удалить cookie с именем " + cookieName + " ?")) {
-                document.cookie = item + "; " + "path=/; expires=" + date.toUTCString();
-                tbody.removeChild(row);
-            }
-        });
     });
+
+    function _onRemoveButtonClick(e) {
+        if (e.target.classList.contains('del-button')) {
+            var button = e.target;
+            var cookieName = button.getAttribute('data-cookie-name');
+            var cookieRow = button.parentNode.parentNode;
+            var date = new Date(0);
+            if (confirm("Удалить cookie с именем " + cookieName + "?")) {
+                document.cookie = cookieName + "=; " + "path=/; expires=" + date.toUTCString();
+                cookieRow.parentNode.removeChild(cookieRow);
+            }
+        }
+    };
+
+    tbody.addEventListener('click', _onRemoveButtonClick);
+
 });
